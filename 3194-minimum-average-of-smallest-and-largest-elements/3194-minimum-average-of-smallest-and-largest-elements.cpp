2aafe0ff3fa392da835b759383e3027fb46889bd
leela venkat <126691297+leelavenkat12@users.noreply.github.com>
2026-07-21 21:49:04 +0530

    Time: 0 ms (100%), Space: 25.7 MB (60.85%) - LeetHub

---
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double minimumAverage(vector<int>& nums) {
+       sort(nums.begin(),nums.end());
+       int left = 0;
+       int right = nums.size()-1;
+        double ans = INT_MAX;
+       while(left<right){
+        double avg = (nums[left]+nums[right])/2.0;
+        ans = min(ans,avg);
+        left++;
+        right--;
+       } 
+       return ans;
+    }
+};
