6f639aeb49bef4b87f1417ffcb5290c2dd4215b1
leela venkat <126691297+leelavenkat12@users.noreply.github.com>
2026-08-07 23:07:34 +0530

    Time: 0 ms (100%), Space: 10 MB (36.55%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        int left = 0;
-        int right = s.size()-1;
-        while(left < right){
-           char singledigitofleft = tolower(s[left]);
-           char singledigitofright = tolower(s[right]);
-           if(singledigitofleft == singledigitofright){
-            left++;
-            right--;
 
-           }
-           else if(!isalnum(s[left])){
-            left++;
-
-           }
-           else if(!isalnum(s[right])) {
-            right--;
-           }
-           else{
-            return false;
-           }
+bool isPalindrome(string s) {
+        int start = 0;
+        int end = s.size()-1;
+        while(start < end ){
+            if(!isalnum(s[start])){ 
+                start++; 
+            continue;
+            }
+            if(!isalnum(s[end])) {
+                end--; 
+            continue;
+            }
+            if(tolower(s[start]) != tolower(s[end])){
+                return false;
+            }
+            start++;
+            end--;
         }
         return true;
-            }
+    }
 };
